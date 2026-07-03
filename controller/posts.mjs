87f0758778042e9f294db74189455d351955f47a
배성욱 <qotjddnr1120@gmail.com>
2026-07-03 15:14:44 +0900

    포스트 변경 API 완료

--- controller/posts.mjs
+++ controller/posts.mjs
@@ -26,3 +26,30 @@ export async function getPost(req, res) {
     }
     
 }
+// 포스트 수정하기
+export async function updatePost(req, res) {
+    const id = req.params.id
+    const text = req.body.text
+    const post = await postRepository.getById(id)
+    if(!post) {
+       return res.status(404).json({ message: `${id}의 포스트가 없습니다`})
+    }
+    if(post.idx !== req.id){ 
+        return res.sendStatus(403)
+    }
+    const updated = await postRepository.update(text, id)
+    res.status(200).json(updated)
+}
+// 포스트 삭제하기
+export async function deletePost(req, res) {
+    const id = req.params.id
+    const post = await postRepository.getById(id)
+    if(!post) {
+       return res.status(404).json({ message: `${id}의 포스트가 없습니다`})
+    }
+    if(post.idx !== req.id){ 
+        return res.sendStatus(403)
+    }
+    await postRepository.remove(id)
+    res.sendStatus(204)
+}
--- data/posts.mjs
+++ data/posts.mjs
@@ -24,10 +24,30 @@ export async function getAll() {
 export async function getAllByUserid(userid) {
     return getPosts().find({ userid }).sort({ createdAt : -1 }).toArray()
 }
-
+// id로 포스트 가져오기
 export async function getById(id) {
     return getPosts().find({ _id: new ObjectId(id) }).next().then(mapOptionalPost)
 }
+// 포스트 수정하기
+export async function update(text, id) {
+    return getPosts().findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { 
+            $set: {
+                text: text.trim()    
+            }
+        },
+        {
+            // 변경되고나서 이전값, 변경된값을 보여주는 코드
+            returnDocument: "after" // 변경된 값을 보여준다
+        }
+    )}
+
+// 포스트 삭제
+export async function remove(id) {
+    return getPosts().deleteOne({ _id: new ObjectId(id) })
+}
+    
 
 function mapOptionalPost(post) {
     return post ? { ...post, id: post._id.toString() } : post
