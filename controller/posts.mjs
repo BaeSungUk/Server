e89af52f4c4d31c016ea8a984daec294d4d8aa6b
배성욱 <qotjddnr1120@gmail.com>
2026-07-03 12:09:59 +0900

    id로 포스트 가져오기 API 완료

--- controller/posts.mjs
+++ controller/posts.mjs
@@ -7,3 +7,22 @@ export async function createPost(req, res) {
     const post = await postRepository.create(text, req.id)
     res.status(201).json(post)
 }
+
+// 모든 포스트를 가져오는 함수
+export async function getPosts(req, res) {
+    const userid = req.query.userid
+    const data = await (userid ? postRepository.getAllByUserid(userid) : postRepository.getAll())
+    res.status(200).json(data)
+}
+
+// id로 포스트를 가져오는 함수
+export async function getPost(req, res) {
+    const id = req.params.id
+    const post = await postRepository.getById(id)
+    if(post) {
+        res.status(200).json(post)
+    }else{
+        res.status(404).json({ message: `${id}의 포스트가 없습니다.`})
+    }
+    
+}
--- middleware/auth.mjs
+++ middleware/auth.mjs
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken"
 import { config } from "../config.mjs"
 import * as authRepository from "../data/auth.mjs"
 
-
 const AUTH_ERROR = { message: "인증에러" }
 
 // next는 맨 끝에 next()를 하게되면 실행한 곳에서 그다음 실행할 부분을 이어서 실행 시킴
